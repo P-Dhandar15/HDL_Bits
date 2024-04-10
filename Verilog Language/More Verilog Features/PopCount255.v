6172437ab212eeb5ae1ebdf2702be28ad83c9010
Pranav Dhandar <130864863+P-Dhandar15@users.noreply.github.com>
2024-04-10 12:19:27 +0530

    Create PopCount255.v

---
+++ Verilog Language/More Verilog Features/PopCount255.v
@@ -0,0 +1,23 @@
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    
+    integer i;
+    reg [7:0] count;
+    
+    always@(*)
+        begin
+            count = 0;
+            
+            for(i = 0; i<255; i++)
+        		begin
+                    if(in[i] == 1'b1)
+                        begin
+                            count = count + 1'b1;
+                        end
+        		end
+            out = count;
+        end
+    
+
+endmodule
